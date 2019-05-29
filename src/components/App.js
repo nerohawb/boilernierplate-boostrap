1dc2a3fb082afd52b056ac69698c06b7ade532f4
Nierc Javinal <nieric93@gmail.com>
2019-05-29 10:03:38 +0800

    First Commit

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import NavBar from './navbar';
+
+class App extends Component {
+	render() {
+		return <div>
+			<header>
+				<NavBar />
+			</header>
+
+		</div>
+	}
+}
+
+export default App;
